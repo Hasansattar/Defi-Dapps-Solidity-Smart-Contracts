5b9bb21aa0a49f4a41c0355e428f1bddd2923f5d
Hasansattar <hasansattar650@gmail.com>
2021-10-06 14:52:06 +0500

    step02_hardhat_simple_storage

---
+++ step02_hardhat_simple_storage/test/sample-test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("SimpleStorage", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Storage = await ethers.getContractFactory("SimpleStorage");
+    const storage = await Storage.deploy();
+    await storage.deployed();
+
+      const setUpdateTx    =await storage.updateData(10);
+
+    // wait until the transaction is mined
+    await setUpdateTx.wait();
+
+    expect(await storage.readData()).to.equal(10);
+  });
+});
